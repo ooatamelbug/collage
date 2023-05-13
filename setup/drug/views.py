3d2dd392279e012e4dd57169cd8a51ccda042189
ooatamelbug <mohamedwork636@gmail.com>
2023-05-13 22:56:02 +0200

    edit in stock

--- setup/drug/views.py
+++ setup/drug/views.py
@@ -46,20 +46,18 @@ class DrugApi(APIView):
             return Response(data={"message": "not found"}, status=401)
 
     def post(self, request):
-        en_brand_name = request.data.get('en_brand_name')
-        if en_brand_name is not None:
-            en_brand_name.upper()
-        chemical_name = request.data.get('chemical_name')
-        national_code = request.data.get('national_code')
-        class_id = request.data.get('class_id')
-        if en_brand_name:
-            data = Drug.objects.filter(en_brand_name__contains=en_brand_name)
-        elif chemical_name:
-            data = Drug.objects.filter(chemical_name__contains=chemical_name)
-        elif national_code:
-            data = Drug.objects.filter(national_code__contains=national_code)
-        elif class_id:
-            data = Drug.objects.filter(class_id=class_id)
+        key = request.data.get('key')
+        value = request.data.get('value')
+        if key == 'en_brand_name':
+            value.upper()
+        if key == 'en_brand_name':
+            data = Drug.objects.filter(en_brand_name__contains=value)
+        elif key == 'chemical_name':
+            data = Drug.objects.filter(chemical_name__contains=value)
+        elif key == 'national_code':
+            data = Drug.objects.filter(national_code__contains=value)
+        elif key == 'class_id':
+            data = Drug.objects.filter(class_id=value)
         else:
             data = Drug.objects.all()
 
